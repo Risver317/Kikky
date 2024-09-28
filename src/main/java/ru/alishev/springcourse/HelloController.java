d8cbc043e92d7a6968f20ac8d25ac617aaafd21d
Misha <misha-almazov@mail.ru>
2024-09-28 22:55:27 +0300

    Hello kitty

--- src/main/java/ru/alishev/springcourse/HelloController.java
+++ src/main/java/ru/alishev/springcourse/HelloController.java
@@ -10,6 +10,8 @@ public class HelloController {
     @GetMapping("/hello-world")
     public String sayHello() {
         String hello = "Nikita moy pasinok";
+        System.out.println("SOSAT9");
         return "hello_world";
+
     }
 }
